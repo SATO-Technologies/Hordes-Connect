80be4d3aac5b6d79d474911c9e7f7ca7edcb3a0b
damiandizeo <damiandizeo@gmail.com>
2023-06-01 17:48:42 -0300

    refactor (beta)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 /* contants */
 const connectUrlBase = 'https://hordesconnect.bysato.com';
+const apiUrlBase = 'https://api.bysato.com/hordesconnect';
 
 /* utils */
 import { isMobile } from './utils/device';
@@ -29,8 +30,27 @@ class HordesConnect {
   }
 
   /* actions */
-  open(params = { }, callback) {
-    let hordesConnectWindow = window.open(`${connectUrlBase}?payload=${encodeURIComponent(JSON.stringify(params))}`, '_blank', `toolbar=no,scrollbars=no,resizable=no,top=${this.size.top},left=${this.size.left},width=${this.size.width},height=${this.size.height}}`);
+  async createRequest(params) {
+    try {
+      let response = await fetch(`${apiUrlBase}/request.php`, {
+        method: 'POST',
+        body: JSON.stringify(params),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      response = await response.json();
+      if( response && response.requestId ) {
+        return response.requestId;
+      }
+      return null;
+    } catch {
+      return null;
+    }
+  }
+
+  open(requestId, callback) {
+    let hordesConnectWindow = window.open(`${connectUrlBase}/${requestId}`, '_blank', `toolbar=no,scrollbars=no,resizable=no,top=${this.size.top},left=${this.size.left},width=${this.size.width},height=${this.size.height}}`);
     window.addEventListener('message', (e) => {
       let origin = e.originalEvent?.origin || e.origin;
       if( origin.includes(connectUrlBase) ) {
@@ -39,22 +59,27 @@ class HordesConnect {
     });
   }
 
-  getAddress({ message = 'Address and Utxos' } = { }) {
+  getAddress({ message = 'Address' } = { }) {
     return new Promise(async (resolve, reject) => {
       try {
-        this.open({
+        let requestId = await this.createRequest({
           app: this.app,
-          type: 'address_utxos',
+          type: 'address',
           message: message,
-        }, (data) => {
-          if( data && data.type == 'address_utxos' ) {
-            resolve(data.payload);
-          } else {
-            resolve(null);
-          }
         });
-      } catch {
-        reject(null);
+        if( requestId ) {
+          this.open(requestId, (data) => {
+            if( data && data.type == 'address' ) {
+              resolve(data.payload);
+            } else {
+              reject({ error: 'unable to get address' });
+            }
+          });
+        } else {
+          reject({ error: 'unable to create request' });
+        }
+      } catch (e) {
+        reject({ error: 'unable to get address' });
       }
     });
   }
@@ -62,21 +87,54 @@ class HordesConnect {
   signTransaction({ message = 'Sign Transaction', base64Psbt = null, broadcast = false }) {
     return new Promise(async (resolve, reject) => {
       try {
-        this.open({
+        let requestId = await this.createRequest({
           app: this.app,
           type: 'sign_transaction',
           message: message,
           base64Psbt: base64Psbt,
           broadcast: broadcast
-        }, (data) => {
-          if( data && data.type == 'sign_transaction' ) {
-            resolve(data.payload)
-          }
         });
+        if( requestId ) {
+          this.open(requestId, (data) => {
+            if( data && data.type == 'sign_transaction' ) {
+              resolve(data.payload)
+            } else {
+              reject({ error: 'unable to sign transaction' });
+            }
+          });
+        } else {
+          reject({ error: 'unable to create request' });
+        }
       } catch {
-        reject({
-          error: 'unable to sign transaction'
+        reject({ error: 'unable to sign transaction' });
+      }
+    });
+  }
+
+  mint({ message = 'Mint Inscription', commitmentPsbt, revealTxData, fee }) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let requestId = await this.createRequest({
+          app: this.app,
+          type: 'mint_inscription',
+          message: message,
+          commitmentPsbt: commitmentPsbt,
+          revealTxData: revealTxData,
+          fee: fee
         });
+        if( requestId ) {
+          this.open(requestId, (data) => {
+            if( data && data.type == 'mint_inscription' ) {
+              resolve(data.payload)
+            } else {
+              reject({ error: 'unable to mint inscription' });
+            }
+          });
+        } else {
+          reject({ error: 'unable to create request' });
+        }
+      } catch (e) {
+        reject({ error: 'unable to mint inscription' });
       }
     });
   }
